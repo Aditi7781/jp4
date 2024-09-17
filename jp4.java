d76bf59a2bf557e4a5f8bf7f530c076993bb9ac0
Aditi7781 <156985565+Aditi7781@users.noreply.github.com>
2024-09-17 23:09:38 +0530

    Add files via upload

---
+++ jp4.java
@@ -0,0 +1,14 @@
+public class jp4 {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows in the pattern
+
+        for (int i = 1; i <= rows; i++) {
+            // Print stars in each row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            // Move to the next line after printing each row
+            System.out.println();
+        }
+    }
+}
